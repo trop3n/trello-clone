1e400b5795f5ab9e4452163fe9e6cf196a198701
trop3n <jasondkimm@proton.me>
2025-04-13 22:04:16 -0500

    added button actions, html, and styles

--- src/components/Board.js
+++ src/components/Board.js
@@ -41,5 +41,38 @@ const Board = () => {
             alignItems: 'center',
             justifyContent: 'center',
         },
-    }
+    };
+
+    return (
+        <div className="board-container">
+            <div className="board">
+            </div>
+            <div className="board-button">
+                <button data-testid='list-add' className="list-add" onClick={() => openModal()}>
+                    <AddIcon style={{ fontSize: "2em", color: "black" }}/>
+                    <h2 className="list-add-text"> Create a new list </h2>
+                </button>
+            </div>
+            <Modal
+                    isOpen={isModalOpen}
+                    ariaHideApp={false}
+                    onRequestClose={() => closeModal()}
+                    style={modalStyles}
+                    testId='list-add-modal'
+                >
+                    <CloseIcon data-testid='list-add-modal-close' style={{ fontSize: "40px", color: "red", position: "absolute", top: 0, right: 0, padding: "10px" }} onClick={() => closeModal()}/>
+                    <input
+                        type="text"
+                        value={modalText}
+                        onChange={(event) => setModalText(event.target.value)}
+                        placeholder="Enter list name"
+                        className="modal-input"
+                        data-testid='list-add-input'
+                    />
+                    <button data-testid='create-list-button' className="modal-button" onClick={() => addList()}> Create list </button>
+            </Modal>
+        </div>
+    );
 }
+
+export default Board;
