42ad764d63935880cecf0a1ffdaed56ccd12f72a
trop3n <jasondkimm@proton.me>
2025-04-13 21:55:45 -0500

    added constants, modalStyles

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,3 +7,39 @@ import { createNewListReducer, dragReducer } from '../boardSlice';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
 import List from '../components/List';
+import { BiLeftArrow } from 'react-icons/bi';
+
+const Board = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalText, setModalText] = useState('');
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => {
+        setModalText('');
+        setIsModalOpen(false);
+    }
+
+    const addList = async () => {
+        if (modalText === "") {
+            window.alert("Please enter a list name");
+            return;
+        }
+        setModalText('');
+        closeModal();
+    }
+
+    const modalStyles = {
+        content: {
+            minWidth: '500px',
+            minHeight: '500px',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+    }
+}
