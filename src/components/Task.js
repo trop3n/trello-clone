347e354f247915650f835c23a79636679b4c3f4e
trop3n <jasondkimm@proton.me>
2025-05-28 22:55:26 -0500

    started work on Task.js file

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,13 +6,37 @@ import { deleteTaskReducer, editTaskReducer } from '../boardSlice';
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import CloseIcon from '@mui/icons-material/Close';
 
-const Task = () => {
+const Task = ({ list, task }) => {
 
-    return (
-        <div>
-            
-        </div>
-    );
-}
+    const dispatch = useDispatch();
+
+    const [modalTaskValue, setModalTaskValue] = useState(task.taskTitle);
+    const [modalDescriptionValue, setModalDescriptionValue] = useState(task.taskDescription);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalTaskValue(task.taskTitle);
+        setModalDescriptionValue(task.taskDescription);
+        setIsModalOpen(true)
+    };
+    const closeModal = () => {
+        setModalTaskValue(task.taskValue);
+        setModalDescriptionValue(task.taskDescription);
+        setIsModalOpen(false);
+    }
+
+    const editTask = async () => {
+        if (modalTaskValue === "") {
+            window.alert("Task title cannot be empty! Please write title for the task.");
+            return;
+        }
+        dispatch(editTaskReducer(
+            {
+                listId: list.listId,
+                taskId: task.taskID,
+            }
+        ))
+    }
 
 export default Task;
